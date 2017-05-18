38d93beb429825fba9a71564a5f26661dbdc8059
Luis G. Diaz Rodriguez <luisg.diazpr@gmail.com>
2017-05-17 20:37:58 -0400

    Directory space manager
    shell script that will manage the directory's space

---
+++ dir_manager.sh
@@ -0,0 +1,31 @@
+#! /bin/bash 
+
+# default
+SIZEGB=1
+SIZEMB=20000
+
+manageDir()
+{
+  if [ $(du Pictures/reddit_wp/ | awk '{print $1}') -gt $1 ]
+  then
+      rm Pictures/reddit_wp/*
+  fi
+}
+
+
+# only handles integers
+if [ $1 -eq 0 ]
+then 
+    # function with default
+    manageDir $SIZEMB
+elif [ $1 -gt $SIZEGB ]
+then    
+    # function with custom size
+    MAX_SIZE=$[$1 \* 1000000]
+    echo $MAX_SIZE
+
+    manageDir $MAX_SIZE
+else
+    manageDir $SIZEMB
+   
+fi
